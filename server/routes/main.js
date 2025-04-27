26c1afee68d20b523f6c349515c63ec82de1bea4
Mehr-code <papa.molla8@gmail.com>
2025-04-27 22:40:02 +0330

    Create Header + Home content + Footer + styling them

--- server/routes/main.js
+++ server/routes/main.js
@@ -3,7 +3,18 @@ const router = express.Router();
 
 // Routes
 router.get("", (req, res) => {
-  res.send("Hello World");
+  const locals = {
+    title: "Mehr Node.js",
+    description: "This is my First trying to code backed with Node.js",
+  };
+  res.render("index", { locals });
+});
+
+router.get("/about", (req, res) => {
+  const locals = {
+    title: "am I Good Friend?",
+  };
+  res.render("about", { locals });
 });
 
 module.exports = router;
